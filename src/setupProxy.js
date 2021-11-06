54317cb46dd547d3d18812d1da584b5dce1618e5
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-11-06 14:37:05 -0700

    proxy target changes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,10 @@ module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: 'https://travelplanetserver.herokuapp.com/',
+            target: 'http://localhost:5000',
             changeOrigin: true,
         })
     );
 };
+
+//'https://travelplanetserver.herokuapp.com/'

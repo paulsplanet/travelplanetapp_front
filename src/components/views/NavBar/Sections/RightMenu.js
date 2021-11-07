72b280b66ca655e25b86ce3aedb8a77320cc81df
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-11-06 20:32:39 -0700

    update #11

--- src/components/views/NavBar/Sections/RightMenu.js
+++ src/components/views/NavBar/Sections/RightMenu.js
@@ -29,51 +29,51 @@ function RightMenu(props) {
   console.log('success', success)
   console.log('array', Object.keys(user), Object.keys(user).length)
 
-if (success.length > 1) {
-  return (
-    <Menu mode={props.mode}>
-      <Menu.Item key="history">
-        <a href="/history">History</a>
-      </Menu.Item>
+  if (success.length < 2) {
+    return (
+      <Menu mode={props.mode}>
+        <Menu.Item key="mail">
+          <a href="/login">Signin</a>
+        </Menu.Item>
+        <Menu.Item key="app">
+          <a href="/register">Signup</a>
+        </Menu.Item>
+      </Menu>
+    )
+  } else {
+    return (
+      <Menu mode={props.mode}>
+        <Menu.Item key="history">
+          <a href="/history">History</a>
+        </Menu.Item>
 
-      <Menu.Item key="upload">
-        <a href="/product/upload">Upload</a>
-      </Menu.Item>
+        <Menu.Item key="upload">
+          <a href="/product/upload">Upload</a>
+        </Menu.Item>
 
-      <Menu.Item key="mypick">
-        <a href="/user/mypick" style={{ display: 'block', height: '60px', marginRight: '-14px', position: 'relative' }}>
-          <Icon type="heart" style={{ fontSize: 25 }}/>
-          { user.userData && user.userData.mypick[0] &&
-            <div style={{ borderRadius: '3px', height: '6px', width: '6px', backgroundColor:  'tomato', position: 'absolute', top: '12px', right: '22px' }}></div>
-          } 
-        </a>
-      </Menu.Item>
-
-      <Menu.Item key="cart" style={{ paddingBottom: 3 }}>
-        <Badge count={user.userData && user.userData.cart.length}>
-          <a href="/user/cart" className="head-example" style={{ marginRight: -22, color: '#667777'}}>
-            <Icon type="shopping-cart" style={{ fontSize: 30, marginBottom: 3 }} />
+        <Menu.Item key="mypick">
+          <a href="/user/mypick" style={{ display: 'block', height: '60px', marginRight: '-14px', position: 'relative' }}>
+            <Icon type="heart" style={{ fontSize: 25 }}/>
+            { user.userData && user.userData.mypick[0] &&
+              <div style={{ borderRadius: '3px', height: '6px', width: '6px', backgroundColor:  'tomato', position: 'absolute', top: '12px', right: '22px' }}></div>
+            } 
           </a>
-        </Badge>
-      </Menu.Item>
+        </Menu.Item>
 
-      <Menu.Item key="logout">
-        <a onClick={logoutHandler}>Logout</a>
-      </Menu.Item>
-    </Menu>
-  )
-} else {
-  return (
-    <Menu mode={props.mode}>
-      <Menu.Item key="mail">
-        <a href="/login">Signin</a>
-      </Menu.Item>
-      <Menu.Item key="app">
-        <a href="/register">Signup</a>
-      </Menu.Item>
-    </Menu>
-  )
-}
+        <Menu.Item key="cart" style={{ paddingBottom: 3 }}>
+          <Badge count={user.userData && user.userData.cart.length}>
+            <a href="/user/cart" className="head-example" style={{ marginRight: -22, color: '#667777'}}>
+              <Icon type="shopping-cart" style={{ fontSize: 30, marginBottom: 3 }} />
+            </a>
+          </Badge>
+        </Menu.Item>
+
+        <Menu.Item key="logout">
+          <a onClick={logoutHandler}>Logout</a>
+        </Menu.Item>
+      </Menu>
+    )
+  }
 
 
 }
